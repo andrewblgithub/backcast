8d393aec669d32a01d6aebe39ca6a56ef51bfe51
andrewblgithub <andrewblichtenstein@gmail.com>
2017-12-04 20:02:12 -0500

    linted

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -3,7 +3,7 @@ var VideoListView = Backbone.View.extend({
   initialize: function() {
     this.collection.on('sync', function() {
       this.render();  
-    }, this)
+    }, this);
   
   },
 
@@ -13,13 +13,13 @@ var VideoListView = Backbone.View.extend({
     this.$el.find('.video-list').html('');
     this.collection.each(function(video) {
       this.renderVideos(video);
-    }, this)
+    }, this);
     return this.$el;
   },
 
-  renderVideos : function(video){
-    var videoView = new VideoListEntryView({model: video})
-    this.$el.find('.video-list').append(videoView.render())
+  renderVideos: function(video) {
+    var videoView = new VideoListEntryView({model: video});
+    this.$el.find('.video-list').append(videoView.render());
   },
 
   template: templateURL('src/templates/videoList.html')
